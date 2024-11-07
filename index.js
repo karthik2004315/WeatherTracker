5d35664cefa2813fd062d3299b4b25ba0f55bf34
D. S.S.V Karthik <21131A0539@gvpce.ac.in>
2024-11-07 19:16:07 +0530

    Update index.js23

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 document.getElementById('getApi').addEventListener('click', getApi);
-const apiId= 'f7588f03c7354807a1b133358240711';
-function getApi() {
-    var CityName = document.getElementById('cityName').value;
-console.log(CityName)
-const url = `http://api.weatherapi.com/v1/current.json?key=${apiId}&q=${CityName}&aqi=yes`;
-console.log(url);
-fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-        let output = `
-            <div>
-                <div style="color:black; width:15rem; background-color:white; border-radius: 10px;" class="p-3">        
-                    <p class="m-0" style="font-width:bold; font-size:2rem;">${data.location.name}</p>
-                    <p class="m-0" style="font-width:bold; font-size:4rem;"> ${data.current.temp_c}<sup>°C</sup></p>
-                </div>
-            </div>
-            `;
-        console.log('printing data', data);
-        document.getElementById('output').innerHTML = output;
-    }).catch((err) => console.log(err))
-}
+
+        const apiId = 'f7588f03c7354807a1b133358240711';
+
+        function getApi() {
+            var CityName = document.getElementById('cityName').value;
+            console.log("City Name:", CityName);
+
+            const url = `http://api.weatherapi.com/v1/current.json?key=${apiId}&q=${CityName}&aqi=yes`;
+            console.log("API URL:", url);
+
+            fetch(url)
+                .then((res) => {
+                    if (!res.ok) {
+                        throw new Error("Network response was not ok");
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    console.log('Data:', data);
+                    let output = `
+                        <div>
+                            <div style="color:black; width:15rem; background-color:white; border-radius: 10px;" class="p-3">        
+                                <p class="m-0" style="font-weight:bold; font-size:2rem;">${data.location.name}</p>
+                                <p class="m-0" style="font-weight:bold; font-size:4rem;">${data.current.temp_c}<sup>°C</sup></p>
+                            </div>
+                        </div>
+                    `;
+                    document.getElementById('output').innerHTML = output;
+                })
+                .catch((err) => {
+                    console.error("Fetch error:", err);
+                    document.getElementById('output').innerHTML = "<p style='color: red;'>City not found or API error. Please try again.</p>";
+                });
+        }
